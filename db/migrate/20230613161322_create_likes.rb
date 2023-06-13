266d659db394b74c62e52045fe922cabc17c4b38
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-13 12:12:26 -0600

    fix linters

--- db/migrate/20230613161322_create_likes.rb
+++ db/migrate/20230613161322_create_likes.rb
@@ -1,11 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :likes do |t|
-
-      t.timestamps
-    end
+    create_table :likes, &:timestamps
     add_reference :likes, :user, foreign_key: true
     add_reference :likes, :post, foreign_key: true
-
   end
 end
--- db/migrate/20230613161719_create_comments.rb
+++ db/migrate/20230613161719_create_comments.rb
@@ -8,6 +8,5 @@ class CreateComments < ActiveRecord::Migration[7.0]
 
     add_reference :comments, :user, foreign_key: true
     add_reference :comments, :post, foreign_key: true
-
   end
 end
