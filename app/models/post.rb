916e8f7827643ac056a108aafe4592f928b60afc
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-14 14:16:12 -0600

    Created validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'users_id'
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
 
@@ -9,12 +9,14 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
   private
   def increment_user_posts_counter
     user.increment!(:posts_counter)
   end
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
+
 end
