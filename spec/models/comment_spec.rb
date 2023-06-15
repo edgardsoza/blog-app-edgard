f339b22a2042df1f51951e31aea380b59ebda22e
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-15 15:07:51 -0600

    Tests Created for all users

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let(:user) { User.create(name: 'User Test', photo: 'photo.com', bio: 'Test Comment', posts_counter: 0) }
+  let(:post) do
+    Post.create(author_id: user.id, title: 'Test', text: 'Text Test', comments_counter: 0,
+                likes_counter: 0)
+  end
+
+  it 'belongs to a user' do
+    expect(Comment.reflect_on_association(:user).macro).to eq(:belongs_to)
+  end
+
+  it 'belongs to a post' do
+    expect(Comment.reflect_on_association(:post).macro).to eq(:belongs_to)
+  end
+
+  it 'increments the comments counter of the associated post after creation' do
+    expect {
+      Comment.create(user: user, post: post)
+    }.to change { post.reload.comments_counter }.by(1)
+  end
+
+  it 'increments the comments counter of the associated post when the comment is saved' do
+    post.comments_counter = 0
+    comment = Comment.new(user: user, post: post)
+    comment.save
+
+    expect(post.reload.comments_counter).to eq(1)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Always Valid Test" do
-  subject { User.new(name: 'Test User', photo: 'www.photo.com', bio: 'Test User model') }
+RSpec.describe User, type: :model do
+  let(:user) { User.new(name: 'Test User', photo: 'photo.com', bio: 'Test User') }
 
-  before { subject.name = nil; subject.save }
-  
-  it 'name should be present' do
-    expect(subject).to_not be_valid
+  before { user.save }
+
+  it 'validates presence of name' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'validates that posts_counter is greater than or equal to 0' do
+    user.posts_counter = -10
+    expect(user).to_not be_valid
+  end
+
+  it 'validates that posts_counter is greater than or equal to 0' do
+    user.posts_counter = 10
+    expect(user).to be_valid
+  end
+
+  it 'returns the last three posts for the user' do
+    expect(user.recent_posts).to eq([])
   end
 end
-
