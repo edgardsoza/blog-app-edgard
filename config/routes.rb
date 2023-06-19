63a0c100117ab275d9271b19da80e16032f81ae2
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-19 14:42:35 -0600

    Implement reviewer recommendations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'users/:id', to: 'users#show'
-  get 'users/:id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:post_id', to: 'posts#show'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   root "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
