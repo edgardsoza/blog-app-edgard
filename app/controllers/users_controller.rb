84c794ddbd59784f9ddd724dc49b313c0e1cb6a5
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-19 14:29:59 -0600

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-    def show
-      # Placeholder logic or retrieve the user with the given ID from the database
-      @user = User.find(params[:id])
-    end
+  def show
+    # Placeholder logic or retrieve the user with the given ID from the database
+    @user = User.find(params[:id])
   end
-  
+end
