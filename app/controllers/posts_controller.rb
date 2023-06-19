505b3592925c5c2f64b46de3575627b05493a4ca
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-19 14:32:52 -0600

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
   def index
-    # Placeholder logic or retrieve all posts for the user with the given ID from the database
     @user = User.find(params[:id])
     @posts = @user.posts
   end
 
   def show
-    # Placeholder logic or retrieve the post with the given post_id for the user with the given user_id from the database
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id])
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    # Placeholder logic or retrieve the user with the given ID from the database
     @user = User.find(params[:id])
   end
 end
