4b027d16d080e7efb3d4302b198e3b54110a51c8
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-22 06:57:57 -0600

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,13 @@
 class LikesController < ApplicationController
     def create
+      @user = User.find(params[:user_id]) # Add this line to set the @user variable
       @post = Post.find(params[:post_id])
-      @like = @post.likes.build(user: current_user)
+      @like = @post.likes.new(user: current_user)
   
       if @like.save
-        redirect_to @post, notice: 'Liked successfully.'
+        redirect_to user_post_url(@user, @post), notice: 'Liked successfully.'
       else
-        redirect_to @post, alert: 'Failed to like the post.'
+        redirect_to user_post_url(@user, @post), alert: 'Failed to like the post.'
       end
     end
   end
