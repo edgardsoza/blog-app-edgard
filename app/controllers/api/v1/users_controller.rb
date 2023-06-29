9cf16bb8596eed39ec65956ae9e8bb747885db4f
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-29 17:00:52 -0600

    Created the controllers and routes set up as needed

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,49 @@
+module Api
+    module V1
+      class UsersController < ApplicationController
+        before_action :set_user, only: [:show, :posts, :comments, :add_comment]
+  
+        def index
+          @users = User.all
+          render json: @users
+        end
+  
+        def show
+          render json: @user
+        end
+  
+        def posts
+          @posts = @user.posts
+          render json: @posts
+        end
+  
+        def comments
+          @comments = Comment.where(post_id: @user.posts.pluck(:id))
+          render json: @comments
+        end
+  
+        def add_comment
+          @post = Post.find(params[:post_id])
+          @comment = @post.comments.new(comment_params)
+          @comment.user = @user
+  
+          if @comment.save
+            render json: @comment, status: :created
+          else
+            render json: { error: 'Failed to create comment' }, status: :unprocessable_entity
+          end
+        end
+  
+        private
+  
+        def set_user
+          @user = User.find(params[:id])
+        end
+  
+        def comment_params
+          params.require(:comment).permit(:text)
+        end
+      end
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [] do
-        resources :posts, only: [:index]
+      resources :users, only: [:index, :show] do
+        member do
+          get 'posts', to: 'users#posts'
+          get 'comments', to: 'users#comments'
+          post 'comments/:post_id', to: 'users#add_comment', as: 'add_comment'
+        end
       end
       resources :posts, only: [] do
         resources :comments, only: [:index, :create]
